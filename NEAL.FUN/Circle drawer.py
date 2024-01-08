27a8e398e72655d01a1d290e64dcce2520805c86
Matthijs Duhoux <ilikeurm1@gmail.com>
2024-01-08 14:46:03 +0100

    Clarified and added a failsafe

--- NEAL.FUN/Circle drawer.py
+++ NEAL.FUN/Circle drawer.py
@@ -15,45 +15,90 @@ def draw_square(t, r, b, l):
     # Mouse up after completing the square
     pyautogui.mouseUp()
 
-Center = (1300, 726)
+# define the center of your circle (this might be a bit off to the left as im using opera gx with a sidebar)
+size = pyautogui.size()
 
-# define x and y and r
-x = Center[0]
-y = Center[1]
-r = 550
+if size[1] == 1080:
+    # 1080p monitors
+    Center = (991, 550)
+    Pass = True
+    r = 350
+elif size[1] == 1440:
+    # 1440p monitors
+    Center = (1300, 726)
+    Pass = True
+    r = 550
+else:
+    pyautogui.alert('Sorry, i dont support this resolution, please change you moonitor to: 1920x1080p or 2560x1440p', 'ERROR', 'I understand')
+    Pass = False
 
-# define points in "square"
-top = (x, y - r)
-bottom = (x, y + r)
-right = (x + r, y)
-left = (x - r, y)
+if Pass:
+    # define x and y and r
+    x = Center[0]
+    y = Center[1]
 
-s(4)
+    # define points in "square"
+    top = (x, y - r)
+    bottom = (x, y + r)
+    right = (x + r, y)
+    left = (x - r, y)
 
-draw_square(top, right, bottom, left)
+    s(4)
+
+    # draw circle
+    draw_square(top, right, bottom, left)
+else:
+    pass
+
+
+"""
+===========================================================================================================================================================================================
+================================================================================= UNDER IS  OTHER VERSION =================================================================================
+===========================================================================================================================================================================================
+"""
 
 # import pyautogui
 # import math
 # from time import sleep as s
 
-# # Center point
-# center_x, center_y = 1300, 726
-# radius = 550
+# # define the center of your circle (this might be a bit off to the left as im using opera gx with a sidebar)
+# size = pyautogui.size()
 
-# # Calculate the points of the circle using trigonometry
-# points = []
-# for angle in range(-90, 271, 90):  # Increase step for more or fewer points
-#     x = center_x + int(radius * math.cos(math.radians(angle)))
-#     y = center_y + int(radius * math.sin(math.radians(angle)))
-#     points.append((x, y))
+# if size[1] == 1080:
+#     # 1080p monitors
+#     Center = (991, 550)
+#     Pass = True
+#     r = 350
+# elif size[1] == 1440:
+#     # 1440p monitors
+#     Center = (1300, 726)
+#     Pass = True
+#     r = 550
+# else:
+#     pyautogui.alert('Sorry, i dont support this resolution, please change you moonitor to: 1920x1080p or 2560x1440p', 'ERROR', 'I understand')
+#     Pass = False
 
-# s(4)
+# if Pass:
+#     # define x and y and r
+#     x = Center[0]
+#     y = Center[1]
 
-# # Move the mouse and draw the circle
-# pyautogui.moveTo(points[0])
-# pyautogui.mouseDown()
+#     # Calculate the points of the circle using trigonometry
+#     points = []
+#     for angle in range(-90, 271, 90):  # Increase step for more or fewer points
+#         point_x = x + int(r * math.cos(math.radians(angle)))
+#         point_y = y + int(r * math.sin(math.radians(angle)))
+#         points.append((point_x, point_y))
 
-# for point in points:
-#     pyautogui.moveTo(point)
+#     s(4)
 
-# pyautogui.mouseUp()
+#     # Move the mouse and draw the circle
+#     pyautogui.moveTo(points[0])
+#     pyautogui.mouseDown()
+
+#     for point in points:
+#         pyautogui.moveTo(point)
+
+#     pyautogui.mouseUp()
+# else:
+#     pass
